5a18525a8f19faa8ed0bd90719519a07ef4943a0
saman batool <samanbatool08@gmail.com>
2019-10-25 03:26:04 +0000

    Done.

--- spec/cartoon_collections_spec.rb
+++ spec/cartoon_collections_spec.rb
@@ -1,68 +1,23 @@
-require "spec_helper"
-
-describe "Cartoon Collections" do
-  describe "#roll_call_dwarves" do
-    it "prints out the 7 dwarfs in a numbered list" do
-      dwarves = ["Dopey", "Grumpy", "Bashful"]
-
-      output = capture_stdout do
-        roll_call_dwarves(dwarves)
+def roll_call_dwarves(array)
+      array.each_with_index do |dwarf, index|
+        puts "#{index + 1}. #{dwarf}"
       end
+end
 
-      # This regex allows for arbitrary characters between
-      # the numbering and the name
-      expect(output).to match(/1.*Dopey/)
-      expect(output).to match(/2.*Grumpy/)
-      expect(output).to match(/3.*Bashful/)
-    end
-  end
+roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
 
-  describe "#summon_captain_planet" do
-    it "returns an array with the same number of elements that it was given" do
-      veggies = ["carrot", "cucumber", "pepper"]
-      result = summon_captain_planet(veggies)
-      expect(result.class).to eq(Array)
-      expect(result.length).to eq(3)
-    end
+def summon_captain_planet(array)
+   array.collect { |i| i.capitalize << "!" } 
+end
 
-    it "capitalizes each element and adds an exclamation mark (test 1)" do
-      fruits = ["apple", "banana", "orange"]
-      result = summon_captain_planet(fruits)
-      ['Apple!', 'Banana!', 'Orange!'].each { |w| expect(result).to include w }
-    end
+summon_captain_planet(["earth", "wind", "fire", "water", "heart"])
 
-    it "capitalizes each element and adds an exclamation mark (test 2)" do
-      veggies = ["carrot", "cucumber", "pepper"]
-      result = summon_captain_planet(veggies)
-      ["Carrot!", "Cucumber!", "Pepper!"].each { |w| expect(result).to include w }
-    end
-  end
+def long_planeteer_calls(calls)
+    calls.any? { |i| i.length > 4 }
+end
 
-  describe "#long_planeteer_calls" do
-    it "returns true if any calls are longer than 4 characters" do
-      calls_long = ["axe", "earth", "wind", "fire"]
-      expect(long_planeteer_calls(calls_long)).to eq(true)
-    end
+long_planeteer_calls(["puff", "go", "two"])
 
-    it "returns false if all calls are 4 characters or less" do
-      calls_short = ["wind", "fire", "tree", "axe", "code"]
-      expect(long_planeteer_calls(calls_short)).to eq(false)
-    end
-
-  end
-
-  describe "#find_the_cheese" do
-    it "returns the first element of the array that is cheese" do
-      contains_cheddar = ["banana", "cheddar", "sock"]
-      expect(find_the_cheese(contains_cheddar)).to eq 'cheddar'
-
-      contains_gouda = ["potato", "gouda", "camembert"]
-      expect(find_the_cheese(contains_gouda)).to eq 'gouda'
-    end
-
-    it "returns nil if the array does not contain a type of cheese" do
-      no_cheese = ["ham", "cellphone", "computer"]
-      expect(find_the_cheese(no_cheese)).to eq nil
-    end
-  end
+def find_the_cheese(array)
+    array.find { |i| i == "cheddar" || i == "gouda" || i == "camembert" }
 end
